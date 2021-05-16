e66549ec0f51b342e2a206f8624748353293883a
Srikrishna <83056007+Srikrishnapsk@users.noreply.github.com>
2021-05-16 13:43:44 +0530

    Plus One.py

---
+++ Plus One.py
@@ -0,0 +1,12 @@
+'''
+Problem: Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+         The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+         You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+'''
+a = list(map(int,input().split(" ")))
+a = list(str(int("".join(map(str,a)))+1))
+print(a)
